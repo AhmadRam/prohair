4d3b1523b8a95497730b16af060fc1d46fec3c58
AhmadRam <a.n.s.r.96@gmail.com>
2024-07-26 11:34:07 +0300

    a

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'https://prohair.com.tr'),
 
-    'asset_url' => env('ASSET_URL', request()->getSchemeAndHttpHost()),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
